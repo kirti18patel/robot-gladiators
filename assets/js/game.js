e358ace4089fc3ff53d0b719092b8ca68532b70f
kirti18patel <kirti18patel@yahoo.com>
2021-04-22 23:40:45 -0400

    fight function added

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,2 +1,7 @@
-window.alert("This is an alert! JavaScript is running!");
-window.alert("This is an alert! JavaScript is running!");
+var playerName=window.prompt("What is your robot's name?");
+
+console.log(playerName);
+function fight(){
+    window.alert("the fight has begun");
+}
+fight();
